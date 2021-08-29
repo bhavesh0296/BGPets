686cbcf0f9f41eafab80692e28e9b07630c2c202
Bhavesh <bhavesh.gupta@techtreeit.com>
2021-08-29 17:15:18 +0530

    code added

---
+++ BGPets/BGPets/Routers/AppDelegateRouter.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegateRouter.swift
+//  BGPets
+//
+//  Created by bhavesh on 29/08/21.
+//  Copyright © 2021 Bhavesh. All rights reserved.
+//
+
+import UIKit
+
+public class AppDelegateRouter: Router {
+    
+}
